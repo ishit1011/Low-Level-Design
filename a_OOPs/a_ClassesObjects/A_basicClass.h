c008fdd6efb64c19fe15160639cec0656d706fd9
ishit <singhishit.06@gmail.com>
2025-03-31 00:09:33 +0530

    classes-objects-concepts

---
+++ a_OOPs/a_ClassesObjects/A_basicClass.h
@@ -0,0 +1,74 @@
+#ifndef A_BASIC_CLASS_H
+#define A_BASIC_CLASS_H
+//  Why to use header guards?
+//  Header guards are used to prevent multiple inclusions of the same header file in a C++ program.
+//  This is important because if a header file is included multiple times, it can lead to redefinition errors
+//  & increase compilation time.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// This is a simple class definition in C++.
+// 1. The class is named "Delivery" and it contains private and public members (Access Modifiers : Private, Public, Protected).
+class Delivery
+{
+private:
+    int packageID;
+    string destination;
+    static vector<string> cities; // Static data member (vector of strings) to store cities
+
+public:
+    string status;
+    // 4. Static data member : belongs to the class rather than an instance.
+    static int totalDeliveries; // Declaration of static data member
+
+    /* 2. Constructor: A special member function that is called when an object of the class is created. */
+    // a) Default constructor (no parameters)
+    // b) Copy constructor ( initializes an object using another object of the same class.) [Delivery obj2(Delivery &obj1)]
+    // c) Parameterised (this one)
+    Delivery(int id, string addr)
+    {
+        if (isServicible(addr)){
+            packageID = id;
+            destination = addr;
+            totalDeliveries++;
+            cout << "Constructor of the delivery... with ID: " << packageID << endl;
+        }
+        else{
+            cout << "Sorry, we do not deliver to " << addr << endl;
+        }
+    }
+
+    void deliver(){
+        if(isServicible(destination)){
+            cout << "Delivering the package..." << endl;
+        }
+    }
+
+    void track(string status){
+        if(isServicible(destination)){
+            cout << "Your package is at " << status << endl;
+        }
+    }
+
+    /* 3. Destructor : Invoked automatically whenever an object is going to be destroyed. */
+    
+    ~Delivery(){
+        if(isServicible(destination)){
+            cout << "\nDestructor of ID " << packageID << " has been delivered to " << destination << endl;
+        }
+    }
+
+    // 5. Static member function : can only access static data members and cannot access non-static data members.
+    static bool isServicible(string inputCity){
+        if (find(cities.begin(), cities.end(), inputCity) != cities.end()){
+            return true;
+        }
+        return false;
+    }
+};
+
+int Delivery ::totalDeliveries = 0; // Static data member initialization
+vector<string> Delivery::cities = {"Mumbai", "Kolkata", "Delhi", "Chandigarh"}; // Static data member initialization
+
+#endif
---
+++ a_OOPs/a_ClassesObjects/Main.cpp
@@ -0,0 +1,72 @@
+#include"A_basicClass.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+enum UberStausOptions{
+    LookingForCab,
+    CabArrived,
+    OnTheWay,
+    Delivered
+};
+
+string getCurrentStatus(UberStausOptions status) {
+    switch (status) {
+        case LookingForCab:
+            return "Looking for a cab";
+        case CabArrived:
+            return "Cab has arrived";
+        case OnTheWay:
+            return "On the way to destination";
+        case Delivered:
+            return "Package delivered";
+        default:
+            return "Unknown status";
+    }
+}
+
+
+
+int main(){
+    // Object : each object gets its own copy of data members and all objects share a single copy of member functions
+    Delivery uber(1,"Jalandhar");
+    srand(time(0)); 
+    string currentStatus;
+    int randomStatus = rand() % 4; 
+    // UberStausOptions randomStatus = static_cast<UberStausOptions>(rand() % 4); : Static casting
+    currentStatus = getCurrentStatus((UberStausOptions)randomStatus);
+    // currentStatus = getCurrentStatus(randomStatus);
+    uber.status = currentStatus;
+    cout<<"The uber status is :"<<uber.status<<endl;
+
+    cout<<"\n";
+    // 1. Dynamic memory allocation using "new" operator
+    Delivery *ptr = new Delivery(2,"Ludhiana"); 
+    /*  2. Pointer Stored in Stack
+     Delivery obj(2,"Ludhiana"); // Static memory allocation
+     Delivery *ptr = &obj;
+     */
+    delete(ptr); // Memory Leak in Heap if not deleted
+
+    cout<<"\n";
+    Delivery amazon(3,"Delhi");
+    string curr = getCurrentStatus(UberStausOptions::OnTheWay);
+    amazon.track(curr);
+    cout<<"\nTotal Deliveries: "<<Delivery::totalDeliveries<<endl;
+
+    if(Delivery ::isServicible("Noida")){
+        cout<<"\nYes, we deliver to Noida."<<endl;
+    }
+    else{
+        cout<<"\nSorry, we do not deliver to Noida."<<endl;
+    }
+
+
+    /*  "this" pointer
+        "this" is a special pointer available inside all non-static member functions of a class. 
+        - implicit pointer
+        - holds the memory address of the current object that is calling the function.
+    */
+    return 0;
+}
